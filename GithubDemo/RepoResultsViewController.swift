aa4c3b2c843f80ea1f68268dd0c440be38645df1
Kesong Xie <kesongxie1993@gmail.com>
2017-01-28 17:18:23 -0800

    Finished github feed and lab for week 3

---
+++ GithubDemo/RepoResultsViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  GithubDemo
+//
+//  Created by Nhan Nguyen on 5/12/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+import UIKit
+import MBProgressHUD
+
+fileprivate let reuseIden = "RepoCell"
+// Main ViewController
+class RepoResultsViewController: UIViewController {
+
+    var searchBar: UISearchBar!
+    var searchSettings = GithubRepoSearchSettings()
+
+    var repos: [GithubRepo]?{
+        didSet{
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //set up tablview view
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.estimatedRowHeight = self.tableView.rowHeight
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        
+        // Initialize the UISearchBar
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+
+        // Add SearchBar to the NavigationBar
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+
+        // Perform the first search when the view controller first loads
+        doSearch()
+    }
+
+    // Perform the search.
+    fileprivate func doSearch() {
+
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+
+        // Perform request to GitHub API to get the list of repositories
+        GithubRepo.fetchRepos(searchSettings, successCallback: { (newRepos) -> Void in
+            self.repos = newRepos
+            MBProgressHUD.hide(for: self.view, animated: true)
+            }, error: { (error) -> Void in
+                if error != nil{
+                    print(error!.localizedDescription)
+                }
+        })
+    }
+}
+
+// SearchBar methods
+extension RepoResultsViewController: UISearchBarDelegate {
+
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true
+    }
+
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(false, animated: true)
+        return true
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchSettings.searchString = searchBar.text
+        searchBar.resignFirstResponder()
+        doSearch()
+    }
+}
+
+
+extension RepoResultsViewController: UITableViewDelegate, UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.repos?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIden, for: indexPath) as!RepoTableViewCell
+        cell.selectionStyle = .none
+        cell.repo = self.repos![indexPath.row]
+        return cell
+    }
+    
+}
---
+++ GithubDemo/RepoTableViewCell.swift
@@ -0,0 +1,61 @@
+//
+//  RepoTableViewCell.swift
+//  GithubDemo
+//
+//  Created by Xie kesong on 1/28/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+import UIKit
+
+class RepoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var avatorImageView: UIImageView!{
+        didSet{
+            self.avatorImageView.layer.cornerRadius = 4.0
+            self.avatorImageView.clipsToBounds = true
+        }
+    }
+    
+    @IBOutlet weak var repoTitleLabel: UILabel!
+    @IBOutlet weak var repoDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var authorLabel: UILabel!
+    
+    @IBOutlet weak var startCountBtn: UIButton!
+    
+    @IBOutlet weak var forkCountBtn: UIButton!
+    
+    var repo: GithubRepo!{
+        didSet{
+            if let urlString = self.repo.ownerAvatarURL, let url = URL(string: urlString){
+                self.avatorImageView.setImageWith(url)
+            }
+            self.repoTitleLabel.text = self.repo.name
+            if let ownerName = self.repo.ownerHandle{
+                self.authorLabel.text = "by " + ownerName
+            }
+            self.repoDescriptionLabel.text = self.repo.repoDescription
+            if let startCount = self.repo.stars{
+                self.startCountBtn.setTitle(String(startCount), for: .normal)
+            }
+            if let forkCount = self.repo.forks{
+                self.startCountBtn.setTitle(String(forkCount), for: .normal)
+            }
+            
+
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
